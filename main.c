5083efe15a7e4914b9bb7fc8fb2792b70e3312df
AnwarRezk <33953443+AnwarRezk@users.noreply.github.com>
2017-12-22 02:42:00 +0200

    Update main.c
    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 
 // if you opened an open cell with right number of flags then another cell opened with the right number of flags it opens the 2nd cell surrounding cells too
 
+
 void delay(clock_t a)
 {
     clock_t start;
@@ -30,7 +31,7 @@ typedef struct {
     int is_question;
 } cell;
 
-int board_created = 0, lost = 0, win = 0, first_move = 1, moves = 0, current_left_cells = 0, flags = 0, questions = 0, mines = 0;
+int board_created = 0, lost = 0, win = 0, first_move = 1, moves = 0, flags = 0, questions = 0, mines = 0;
 
 
 int main()
@@ -38,8 +39,6 @@ int main()
     int i;
     char str[30]="Welcome To Minesweeper !";
     char str1[30]="Are You Ready To Play ?";
-    char ch;
-    char tempstr[10];
     printf(" ");
     for(i=0;str[i]!='\0';i++)
     {
@@ -55,117 +54,11 @@ int main()
     }
     delay(1500);
     system("cls");
-    for(;;)
-    {
-        system("cls");
-        printf("(1)Start a new game ");
-        printf("\n");
-        printf("(2)Show Leaderboard"); //display scores
-        printf("\n");
-        printf("(3)Load Game\n");//open saved file
-        ch=getch();
-        if(ch==49)
-            {
-                system("cls");
-              char ch1;
-              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x08);
-              printf("Choose difficulty:\n");
-              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x03);
-              printf("(1)Easy(3x3)\n");
-              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x05);
-              printf("(2)Medium(6x6)\n");
-              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x04);
-              printf("(3)Hard(9x9)\n");
-              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x09);
-              printf("(4)Custom(mxn)\n");
-              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-              ch1=getch();
-              if(ch1==49);
-              //cols=3,,rows=3
-              if(ch1==50);
-                //cols=6,,rows=6
-              if(ch1==51);
-                  //cols=9,,rows=9
-               if(ch1==52)
-              {
-                  system("cls");
-                srand(time(NULL));
-    int rows, cols, x, y, x_mv, y_mv;
-    char action;
-    printf("Enter cols & rows:\n");
-    scanf("%d %d", &cols, &rows);
-    cell cells[cols][rows];
+    play_game();
 
-    current_left_cells = cols*rows;
-
-    for(y=0; y<rows; y++) {
-        for(x=0; x<cols; x++) {
-            cells[x][y].value = 0;
-            cells[x][y].flags = 0;
-            cells[x][y].is_open = 0;
-            cells[x][y].is_flagged = 0;
-            cells[x][y].is_question = 0;
-        }
-    }
-
-    display_board(rows, cols, (cell*)cells);
-
-    while(!lost && !win) {
-
-        if(!win){
-            printf("Enter Your Move : X Y Action\n");
-            scanf("%d %d %c", &x_mv, &y_mv, &action);
-
-            open_cell(x_mv-1, y_mv-1, action, rows, cols, (cell*)cells);
-        }
-
-        int wn = check_win(rows, cols, (cell*)cells);
-
-        display_board(rows, cols, (cell*)cells);
-
-        if(first_move && action != 'f' && action != 'q') {first_move = 0;}
-
-        if(lost) {
-            printf("\nYOU LOST THE GAME !! WOULD YOU GIVE UP ?");
-            return 0;
-        }
-
-        if(wn){
-            win = 1;
-            display_board(rows, cols, (cell*)cells);
-            printf("\nDID YOU KNOW? THERE ARE MORE THAN 9999999999 PEPOLE WHO FINISHED BIGGER BOARDS IN LESS TIME THAN YOURS!!\n\nJUST KIDDING ,CONGRATULATIONS!");
-            return 0;
-        }
-
-
-    }
-              }
-            }
-            if(ch==50)
-        {
-            system("cls");
-            printf("  PLAYER\t\tSCORE\n");
-            printf("\nTo return to main menu press (enter)");
-            fflush(stdin);
-            gets(tempstr);
-            continue;
-        }
-        if(ch==51)
-    {
-        system("cls");
-        printf("Ok!");
-        printf("\nTo return to main menu press (enter)");
-        fflush(stdin);
-        gets(tempstr);
-        continue;
-    }
-
-    }
     return 0;
-}
 
-
-
+    }
     void create_board(int x, int y, int rows, int cols, cell* cells)
 {
     int rnd_x, rnd_y, i, j;
@@ -220,7 +113,6 @@ void display_board(int rows, int cols, cell* cells) {
                 else{
                     SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 249);
                     printf(" %d ",(cells + x*rows + y)->value);
-                    //printf("   ");
                 }
             } else if((cells + x*rows + y)->is_flagged == 1) {
                 SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 237);
@@ -259,21 +151,21 @@ void open_cell(int x, int y, char action, int rows, int cols, cell* cells) {
             (cells + x*rows + y)->is_question = 1;
             if((cells + x*rows + y)->is_flagged == 1){
                 (cells + x*rows + y)->is_flagged = 0;
-                //flags--;
+                flags--;
             }
-            //questions++;
+            questions++;
     }
     else if(action == 'q' && (cells + x*rows + y)->is_question == 1)
     {
             (cells + x*rows + y)->is_question = 0;
-            //questions--;
+            questions--;
     }
 
     if(action == 'f' && (cells + x*rows + y)->is_open == 0 && (cells + x*rows + y)->is_flagged == 0)
     {
             (cells + x*rows + y)->is_flagged = 1;
             (cells + x*rows + y)->is_question = 0;
-            //flags++;
+            flags++;
 
             for(j = -1; j <= 1; j++)
                 for(i = -1; i <= 1; i++)
@@ -284,7 +176,7 @@ void open_cell(int x, int y, char action, int rows, int cols, cell* cells) {
     else if ((action == 'f' || action == 'q') && (cells + x*rows + y)->is_flagged == 1)
     {
             (cells + x*rows + y)->is_flagged = 0;
-            //flags--;
+            flags--;
 
             for(j = -1; j <= 1; j++)
                 for(i = -1; i <= 1; i++)
@@ -292,6 +184,7 @@ void open_cell(int x, int y, char action, int rows, int cols, cell* cells) {
                         (cells + (x+j)*rows + (y+i))->flags -= 1;
 
     }
+
 
     int old_moves = 0;
 
@@ -305,14 +198,14 @@ void open_cell(int x, int y, char action, int rows, int cols, cell* cells) {
                                 if((cells + (x+j)*rows + y+i)->is_open != 1)
                                 {
                                     open_cell(x+j,y+i, '-',rows,cols,cells);
-                                    /*if((cells + (x+j)*rows + y+i)->is_flagged != 1)
-                                        old_moves++;*/
+                                    if((cells + (x+j)*rows + y+i)->is_flagged != 1)
+                                        old_moves++;
                                 }
             //}
         }
     }
 
-    //if(old_moves != 0) {moves++;}
+    if(old_moves != 0) {moves++;}
 
     if(action == 'f' || action == 'q' || (cells + x*rows + y)->is_flagged == 1 || (cells + x*rows + y)->is_question == 1 || (cells + x*rows + y)->is_open == 1 )
         return;
@@ -331,31 +224,15 @@ void open_cell(int x, int y, char action, int rows, int cols, cell* cells) {
 }
 
 int check_win(int rows, int cols, cell* cells) {
-    int x, y, cells_left = 0, c_flags = 0, c_questions = 0;
-
+    int x, y, cells_left = 0;
 
     for(y=0; y<rows; y++) {
         for(x=0; x<cols; x++) {
             if((cells + x*rows + y)->is_open == 0){
                 cells_left++;
             }
-            if((cells + x*rows + y)->is_flagged == 1){
-                c_flags++;
-            }
-            if((cells + x*rows + y)->is_question == 1){
-                c_questions++;
-            }
         }
     }
-    flags = c_flags;
-    questions = c_questions;
-
-
-    if(current_left_cells != cells_left) {
-        moves++;
-        current_left_cells = cells_left;
-    }
-
 
     if(cells_left == mines)
     {
@@ -370,6 +247,149 @@ int check_win(int rows, int cols, cell* cells) {
     }
 
     return 0;
+}
+
+void PlayG(int cols,int rows)
+{
+    int x, y, x_mv, y_mv;
+    char action;
+    cell cells[cols][rows];
+    for(y=0; y<rows; y++) {
+        for(x=0; x<cols; x++) {
+            cells[x][y].value = 0;
+            cells[x][y].flags = 0;
+            cells[x][y].is_open = 0;
+            cells[x][y].is_flagged = 0;
+            cells[x][y].is_question = 0;
+        }
+    }
+
+    display_board(rows, cols, (cell*)cells);
+
+    while(!lost && !win) {
+
+        if(!win){
+            printf("Enter Your Move : X Y Action\n");
+            scanf("%d %d %c", &x_mv, &y_mv, &action);
+
+            if(action != 'q' && action != 'f' && cells[x_mv-1][y_mv-1].is_open == 0)
+                moves++;
+
+            open_cell(x_mv-1, y_mv-1, action, rows, cols, (cell*)cells);
+        }
+        display_board(rows, cols, (cell*)cells);
+
+        if(first_move && action != 'f') {first_move = 0;}
+
+        if(lost) {
+            printf("\nYOU LOST THE GAME !! WOULD YOU GIVE UP ?");
+        }
+
+        if(check_win(rows, cols, (cell*)cells)){
+            win = 1;
+            display_board(rows, cols, (cell*)cells);
+            printf("\nDID YOU KNOW? THERE ARE MORE THAN 9999999999 PEPOLE WHO FINISHED BIGGER BOARDS IN LESS TIME THAN YOURS!!\n\nJUST KIDDING ,CONGRATULATIONS!");
+        }
+
+    }
+}
+
+void play_game()
+{
+    char ch;
+    char tempstr[10];
+    for(;;)
+    {
+        system("cls");
+        SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x12);
+        printf("===MINESWEEPER===\n");
+        SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        printf("-------------------\n");
+        printf("(1)Start a new game ");
+        printf("\n");
+        printf("(2)Show Leaderboard"); //display scores
+        printf("\n");
+        printf("(3)Load Game\n");//open saved file
+        ch=getch();
+        if(ch==49)
+            {
+                system("cls");
+              char ch1;
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x12);
+              printf("====MINESWEEPER====\n");
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x08);
+              printf("Choose difficulty:\n");
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x03);
+              printf("(1)Easy(3x3)\n");
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x05);
+              printf("(2)Medium(6x6)\n");
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x04);
+              printf("(3)Hard(9x9)\n");
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x09);
+              printf("(4)Custom(mxn)\n");
+              SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+              printf("Press ' Enter ' to get back\n");
+              ch1=getch();
+              if(ch1==49)
+              {
+                  PlayG(3,3);
+
+              }
+              if(ch1==50)
+                {
+                    PlayG(6,6);
+
+                }
+              if(ch1==51)
+                  {
+                      PlayG(9,9);
+
+                  }
+               if(ch1==52)
+              {
+                  int rows,cols;
+                  system("cls");
+                srand(time(NULL));
+    printf("Enter rows & columns:\n");
+    scanf("%d %d", &rows, &cols);
+    checkInt();
+    PlayG(rows,cols);
+              }
+            }
+            if(ch==50)
+        {
+            system("cls");
+            SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x12);
+            printf("===MINESWEEPER===\n");
+            SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+            printf("-----------------------------------\n");
+            printf("  PLAYER\t\tSCORE\n");
+            printf("Press ' Enter ' to get back\n");
+            fflush(stdin);
+            gets(tempstr);
+            continue;
+        }
+        if(ch==51)
+    {
+        system("cls");
+        SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x12);
+        printf("===MINESWEEPER===\n");
+        SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        printf("Ok!\n");
+        printf("Press ' Enter ' to get back\n");
+        fflush(stdin);
+        gets(tempstr);
+        continue;
+    }
+}
+}
+
+void checkInt(int rows,int cols)
+{
+
+char term;
+if(scanf("%d%d%c", &rows, &cols, &term) != 2 || term != '\n')
+    printf("failure\n"); //scan again rows & cols
 }
 
 void clrscr()
